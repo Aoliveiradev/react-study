5f9e90cf51385452000c1d173c28fb59d157c2c3
Aoliveiradev <allandaviddev@gmail.com>
2022-04-01 10:09:33 -0300

    Adicionando suporte a rota

--- src/main/react/src/index.js
+++ src/main/react/src/index.js
@@ -1,9 +1,6 @@
 import { render } from "react-dom";
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
 import App from "./App";
-import Students from "./pages/students-page/students";
-import EditStudents from "./pages/edit-pages/edit-students-page/edit-students-page";
-
 const rootElement = document.getElementById("root");
 render(
     <BrowserRouter>
